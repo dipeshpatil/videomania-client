236c43784cbeb80f6623d3831acb4857b2b4159b
Dipesh Patil <dipeshpatil@Ds-MacBook-Pro.local>
2024-11-09 23:50:31 +0530

    Fix: Router Issues and Navbar

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,10 @@ import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-  <div class="app"></div>
-  <Navbar />
+  <div class="app">
+    <Navbar />
+    <router-view />
+  </div>
 </template>
 
 <script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,32 +1,53 @@
 <template>
   <nav class="flex justify-center space-x-4 p-4 bg-gray-200">
-    <router-link to="/" class="text-blue-500 hover:underline">Home</router-link>
-    <router-link to="/login" class="text-blue-500 hover:underline"
-      >Login</router-link
-    >
-    <router-link to="/register" class="text-blue-500 hover:underline"
-      >Register</router-link
+    <router-link
+      v-if="isAuthenticated"
+      to="/"
+      class="text-blue-500 hover:underline"
+      >Home</router-link
     >
     <router-link
+      v-if="isAuthenticated"
       @click="logoutUser"
       to="/login"
       class="text-blue-500 hover:underline"
       >Logout</router-link
     >
+    <router-link
+      v-if="!isAuthenticated"
+      to="/login"
+      class="text-blue-500 hover:underline"
+      >Login</router-link
+    >
+    <router-link
+      v-if="!isAuthenticated"
+      to="/register"
+      class="text-blue-500 hover:underline"
+      >Register</router-link
+    >
   </nav>
-  <router-view />
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      isAuthenticated: !!localStorage.getItem("token"), // Initial check
+    };
+  },
   methods: {
     logoutUser() {
       localStorage.removeItem("token");
+      this.isAuthenticated = false; // Update authentication status
+      this.$router.push("/login"); // Redirect to login page
     },
   },
-  data() {},
-  watch: {},
-  mounted() {},
+  watch: {
+    // Watch route changes to update isAuthenticated state in case of token change
+    $route(to, from) {
+      this.isAuthenticated = !!localStorage.getItem("token");
+    },
+  },
 };
 </script>
 
